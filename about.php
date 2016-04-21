23c3a32faa7e95d77f78e18271d86f466e6cdb4b
Aaron Oxenrider <theox26@gmail.com>
2016-04-20 20:38:45 -0400

    bootstrap for mobile

--- about.php
+++ about.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML+RDFa 1.1//EN">
-<html class="js" lang="en">
+<!DOCTYPE html>
+<html lang="en">
 <head>
+<?php include 'includes/static.php'; ?>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=yes">
   <title>Conewago Canoe Club</title>
-  <link rel="stylesheet" type="text/css" href="css/main.css" media="screen, tv, projection" />
+  
 </head>
-
 <body>
   <div class="page" id="page">
     <?php include 'includes/top.php'; ?>
--- includes/footer.php
+++ includes/footer.php
@@ -8,3 +8,7 @@
       </p>
     </div>
   </div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="bootstrap/3.3.6/js/bootstrap.min.js"></script>
