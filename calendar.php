8ac3919c82a5a376a90b222163d32a5ecdeb4095
Aaron <theox26@gmail.com>
2016-11-01 19:29:55 -0400

    calendar

--- calendar.php
+++ calendar.php
@@ -4,7 +4,6 @@
 <?php include 'includes/static.php'; ?>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <title>CCC Events Calendar</title>
-  
 </head>
 <body>
     <?php include 'includes/top.php'; ?>
@@ -22,7 +21,8 @@
       <div class="visible-xs-block">
         <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=500&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=1rucadqml2ft27fbumhnp4lqes%40group.calendar.google.com&amp;color=%23125A12&amp;src=vkrabb4c5cct6k9vkqkk2njceo%40group.calendar.google.com&amp;color=%23711616&amp;ctz=America%2FNew_York" style="border-width:0" width="280" height="500" frameborder="0" scrolling="no"></iframe>
       </div>
-
+    <p>Green - Official Club Events</p>
+  	<p>Red - Kayaking Events of Interest</p>
     </div>
   </div>
   <?php include 'includes/footer.php'; ?> 
